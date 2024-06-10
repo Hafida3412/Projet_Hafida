6950608a774f1dd1dc6c7342c25d799b3fa399d1
Hafida3412 <lounsy81@gmail.com>
2024-06-10 10:53:56 +0200

    création de l'entité logement

---
+++ projet_hafida/model/entities/Logement.php
@@ -0,0 +1,104 @@
+<?php
+namespace Model\Entities;
+
+use App\Entity;
+
+/*
+    En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
+*/
+
+final class Annonce extends Entity{
+
+    private $id;
+    private $nbChambre;
+    private $adresse;
+    private $cp;
+    private $ville;
+    private $image;
+
+    public function __construct($data){         
+        $this->hydrate($data);  
+
+}
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+  
+    public function getNbChambre()
+    {
+        return $this->nbChambre;
+    }
+
+   
+    public function setNbChambre($nbChambre)
+    {
+        $this->nbChambre = $nbChambre;
+
+        return $this;
+    }
+
+
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+ 
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+   
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+ 
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+  
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+  
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
