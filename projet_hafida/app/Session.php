d73bbbad031b4ff022132c8b8fe17b3d5d8c8309
Hafida3412 <lounsy81@gmail.com>
2024-06-10 13:53:27 +0200

    modification du fichier session

--- projet_hafida/app/Session.php
+++ projet_hafida/app/Session.php
@@ -29,7 +29,7 @@ class Session{
     /**
     *   met un user dans la session (pour le maintenir connecté)
     */
-    public static function setUtilisateur($utilisateur){
+    public static function setUser($utilisateur){
         $_SESSION["utilisateur"] = $utilisateur;
     }
 
@@ -39,7 +39,7 @@ class Session{
 
     public static function isAdmin(){
         // attention de bien définir la méthode "hasRole" dans l'entité User en fonction de la façon dont sont gérés les rôles en base de données
-        if(self::getUtilisateur() && self::getUtilisateur()->hasRole("ROLE_ADMIN")){
+        if(self::getUser() && self::getUser()->hasRole("ROLE_ADMIN")){
             return true;
         }
         return false;
