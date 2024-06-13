28a83b5f16a72110ac4f2708794e93a4f789bdab
Hafida3412 <lounsy81@gmail.com>
2024-06-13 14:15:41 +0200

    modification de la vue ajoutAnnonces

--- projet_hafida/view/forum/ajoutAnnonces.php
+++ projet_hafida/view/forum/ajoutAnnonces.php
@@ -6,7 +6,8 @@
     
     <?php
   ?> 
-    <form action="index.php?ctrl=forum&action=ajoutAnnonces" method="post"><!--on ne rajoute pas de "id" à ajoutAnnonces pour ne pas insérer l'id de l'utilisateur dans l'Url, trop dangereux.
+    <form action="index.php?ctrl=forum&action=ajoutAnnonces" method="post">
+        <!--on ne rajoute pas de "id" à ajoutAnnonces pour ne pas insérer l'id de l'utilisateur dans l'Url, trop dangereux.-->
         <label for="dateDebut">Date début</label>
         <input type="date" name="dateDebut"><br>
 
