d75abb2f2baf1b868eaaeb3be7935ade9feaf7b1
Hafida3412 <lounsy81@gmail.com>
2024-06-20 16:51:51 +0200

    correction de la vue monCompte

--- projet_hafida/view/forum/detailsUtilisateur.php
+++ projet_hafida/view/forum/detailsUtilisateur.php
@@ -13,23 +13,11 @@ $utilisateur = $result["data"]['utilisateur'];
 <br>
 <p>Mes annonces:</p>
 <?php
-if(!empty($annonces)) {
-    foreach($annonces as $annonce) {
-        echo "<p>"."Annonce de <a href='index.php?ctrl=forum&action=detailsAnnonce&id=".$annonce->getId()."'>"
-        .$annonce->getUtilisateur()->getPseudo()."</a>" ." du "
-        .(date('d-m-Y H:i:s', strtotime($annonce->getDateCreation()))).
-        "<br>Nb de chats: ".$annonce->getNbChat()."<br>Date de début: "
-        (date('d-m-Y', strtotime($annonce->getDateDebut())))."<br>Date de fin: "
-        .(date('d-m-Y', strtotime($annonce->getDateFin())))."<br>Description: "
-        .$annonce->getDescription()."</p><br>";
-    }
-} else {
-    echo "<p>Pas d'annonces à afficher pour le moment.</p>";
-}
 
 ?>
 <br>
 <p>Mes logements:</p>
+<br>
 <?php
         
         
