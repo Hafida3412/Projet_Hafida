a4299375370f7a4da5700edde18a022f608766aa
Hafida3412 <lounsy81@gmail.com>
2024-06-24 14:43:11 +0200

    modification du formulaire de réservation

--- projet_hafida/view/forum/reservationFormulaire.php
+++ projet_hafida/view/forum/reservationFormulaire.php
@@ -1,18 +1,34 @@
 <h1>Réservation</h1>
 <form action="index.php?ctrl=forum&action=reservation" method="post">
+    
+    <label for="nom">Nom</label>
+    <input type="text" name="nom"><br>
+
+    <label for="prenom">Prénom</label>
+    <input type="text" name="prenom"><br>
+
+    <label for="rue">Rue</label>
+    <input type="text" name="rue"><br>
+    
+    <label for="codePostal">Code Postal</label>
+    <input type="text" name="codePostal"><br>
+
+    <label for="ville">Ville</label>
+    <input type="text" name="ville"><br>
+
     <label for="numeroTelephone">Numéro de téléphone</label>
     <input type="text" name="numeroTelephone" id="numeroTelephone"><br>
 
-    <label for="nbAdultes">Nombre d'adultes:</label>
-    <input type="number" name="nbAdultes"><br>
+    <label for="email">Email</label>
+    <input type="text" name="email" id="email"><br>
 
-    <label for="nbEnfants">Nombre d'enfants:</label>
-    <input type="number" name="nbEnfants"><br>
+    <label for="paiement">Mode de Paiement:</label><br>
+    <input type="radio" id="cb" name="paiement" value="cb">
+    <label for="cb">Carte Bancaire (Visa, Mastercard)</label><br>
+    <input type="radio" id="paypal" name="paiement" value="paypal">
+    <label for="paypal">PayPal</label><br>
 
-    <label for="paiement">Paiement:</label>
-    <input type="text" name="paiement"><br>
-
-    <label for="question">Avez-vous des questions?</label>
+    <label for="question">Avez-vous une demande particulière?</label>
     <textarea name="question" rows="4" cols="50"></textarea><br>
 
     <input type="submit" name="submitReservation" value="Réserver">
