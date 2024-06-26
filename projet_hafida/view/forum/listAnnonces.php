a45919526bcca84b71c0907440a85492e17d3fd6
Hafida3412 <lounsy81@gmail.com>
2024-06-26 16:48:49 +0200

    modification de la vue listAnnonces

--- projet_hafida/view/forum/listAnnonces.php
+++ projet_hafida/view/forum/listAnnonces.php
@@ -11,9 +11,13 @@
 
 if($annonces){
 foreach($annonces as $annonce){ 
-    echo "<p>"."Annonce de <a href='index.php?ctrl=forum&action=detailsAnnonce&id=".$annonce->getId()."'>".$annonce->getUtilisateur()->getPseudo()."</a>" ." du ".(date('d-m-Y H:i:s', strtotime($annonce->getDateCreation())))."<br> Nb de chats: ".$annonce->getNbChat()."<br>
+    echo "<p>"."Annonce de <a href='index.php?ctrl=forum&action=detailsAnnonce&id=".$annonce->getId()."'>"
+    .$annonce->getUtilisateur()->getPseudo()."</a>" ." du "
+    .(date('d-m-Y H:i:s', strtotime($annonce->getDateCreation()))).
+    "<br> Nb de chats: ".$annonce->getNbChat()."<br>
     Date de début: ".(date('d-m-Y ', strtotime($annonce->getDateDebut())))."<br>
-    Date de fin: ".(date('d-m-Y ', strtotime($annonce->getDateFin())))."<br> Description: ".$annonce->getDescription()."</p><br>";
+    Date de fin: ".(date('d-m-Y ', strtotime($annonce->getDateFin()))).
+    "<br> Description: ".$annonce->getDescription()."<br> Ville: ".$annonce->getLogement()->getVille()."</p><br>";
 
     // si l'utilisateur est connecté
     if(App\Session::getUtilisateur()) {
