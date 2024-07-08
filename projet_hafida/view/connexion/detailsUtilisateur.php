5cf554b9748fabc62d77f39ab1e8b1465b8c48ed
Hafida3412 <lounsy81@gmail.com>
2024-07-08 17:14:59 +0200

    modification de la vue détailsUtilisateur (mes réservations)

--- projet_hafida/view/connexion/detailsUtilisateur.php
+++ projet_hafida/view/connexion/detailsUtilisateur.php
@@ -22,10 +22,16 @@ $reservations = $result["data"]['reservations'];
 if(isset($reservations) && $reservations){
     foreach($reservations as $reservation){
         // On affiche les détails de chaque réservation
-        echo "<p>Numéro de téléphone: " . $reservation->getNumeroTelephone() .
-        " Nombre d'adultes:" .$reservation->getNbAdultes() ." Nombre d'enfants:". $reservation->getNbEnfants()
-        ." Mode de paiement:" .$reservation->getPaiement() .
-        " Question:" .$reservation->getQuestion() ."</p>"."<br>";
+        echo  
+        "</p>  Nombre de chats:".$reservation->getAnnonce()->getNbChat()."<br>".
+        " Date de début:".$reservation->getAnnonce()->getDateDebut()."<br>".
+        " Date de fin:".$reservation->getAnnonce()->getDateFin()."<br>".
+        " Description: ". $reservation->getAnnonce()->getDescription()."<br>".
+        " Numéro de téléphone: " . $reservation->getNumeroTelephone() ."<br>".
+        " Nombre d'adultes:" .$reservation->getNbAdultes() ."<br>".
+        " Nombre d'enfants:". $reservation->getNbEnfants() ."<br>".
+        " Mode de paiement:" .$reservation->getPaiement() ."<br>".
+        " Question:" .$reservation->getQuestion() ."</p>"."<br>"."<br>";
     }
 } else {
     echo "<p>Aucune réservation pour le moment.</p>";
