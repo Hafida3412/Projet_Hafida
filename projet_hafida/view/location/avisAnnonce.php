d4d53c58748f5a4dcb26d47de9f2ae2228a41432
Hafida3412 <lounsy81@gmail.com>
2024-07-10 11:15:07 +0200

    modification du formulaire Poster un avis

--- projet_hafida/view/location/avisAnnonce.php
+++ projet_hafida/view/location/avisAnnonce.php
@@ -4,8 +4,8 @@
 
 <h1>Poster un avis pour cette annonce</h1>
 <form action="index.php?ctrl=location&action=donnerAvis&id=<?= $annonce_id ?>" method="post">
-    <label for="commentaire">Commentaire</label>
-    <textarea name="commentaire"></textarea>
+    <label for="commentaire">Commentaires:</label><br>
+    <textarea name="commentaire" rows="4" cols="50"></textarea><br>
     <input type="submit" name="submitAvis" value="Poster l'avis">
 </form>
 
