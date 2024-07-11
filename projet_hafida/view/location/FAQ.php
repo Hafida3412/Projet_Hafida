53b64f83d2b5646fa808dff80e69a0d8cc98ccd0
Hafida3412 <lounsy81@gmail.com>
2024-07-11 19:42:45 +0200

    modification de la vue FAQ

--- projet_hafida/view/location/FAQ.php
+++ projet_hafida/view/location/FAQ.php
@@ -1,16 +1,17 @@
 <?php  
   
 ?>
-    <h1>FAQ - Foire aux questions</h1>
+    <h1>FAQ - Foire aux questions</h1><br>
     <p>Voici une liste de questions fréquemment posées :</p>
     
     <ol>
-        <li><strong>Question 1:</strong> Lorem ipsum dolor sit amet?</li>
-        <li><strong>Question 2:</strong> Consectetur adipiscing elit?</li>
-        <li><strong>Question 3:</strong> Sed do eiusmod tempor incididunt?</li>
-        <li><strong>Question 4:</strong> Ullamco laboris nisi ut aliquip ex ea commodo consequat?</li>
-        <li><strong>Question 5:</strong> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore?</li>
-    </ol>
+        <li><strong>Question 1:</strong> Comment m'inscrire?</li>
+        <li><strong>Question 2:</strong> Que faire en cas d'urgence pendant la garde?</li>
+        <li><strong>Question 3:</strong> Suis-je couvert par une assurance?</li>
+        <li><strong>Question 4:</strong> Je n'ai jamais eu de chat, est-ce que je peux quand même m'inscrire?</li>
+        <li><strong>Question 5:</strong> Puis-je à la fois être propriétaire et hôte?</li>
+        <li><strong>Question 6:</strong> Quels sont les modes de paiement?</li>
+        <li><strong>Question 7:</strong> Comment supprimer mon compte?</li><br>
     
     <p>N'hésitez pas à nous contacter pour toute autre question!</p>
 
