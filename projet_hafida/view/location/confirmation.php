5b329b32de72a4d7359c2473aeeafc2497194e7c
Hafida3412 <lounsy81@gmail.com>
2024-07-12 23:04:17 +0200

    modification de la vue confirmation.php

--- projet_hafida/view/location/confirmation.php
+++ projet_hafida/view/location/confirmation.php
@@ -2,11 +2,11 @@
 
 ?>
 <p><strong>Votre réservation est confirmée!</strong></p>
-
+<br>
 <p>Vous allez chez Chouchou à Strasbourg!</p> 
-
+<br>
 <p>Veuillez consulter les informations ci-dessous:</p>
-
+<br>
 <ul>
     <li>Nom: </li>
     <li>Prénom: </li>
@@ -14,12 +14,14 @@
     <li>Date de début: </li>
     <li>Date de fin: </li>
 </ul>
-
+<br>
 <img src="https://media.gettyimages.com/id/1335776677/fr/photo/beautiful-blue-persian-cat-relaxed-on-livingroom.jpg?s=612x612&w=0&k=20&c=EJ1-vXhuEZfquyuSeCH5I0e4DPmwFnpfl8EUA7ljfQY=" alt="Image de Chouchou">
-
+<br>
 <p>Le montant de la réservation est de: </p>
-
+<br>
 <p>Si vous avez des questions ou des modifications à apporter à votre réservation, 
 veuillez nous contacter au plus tôt.</p> 
+<br>
 <p>Nous vous souhaitons un agréable séjour chez Chouchou!</p> 
+<br>
 <p>L'équipe Chez mon Chat.</p>
