0fcba2eb04fb715bc58817520cd8e3b8415c3e0f
Hafida3412 <lounsy81@gmail.com>
2024-07-12 18:19:32 +0200

    création de la vue confirmation.php

---
+++ projet_hafida/view/location/confirmation.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<p>Merci pour votre réservation chez Chouchou à Strasbourg! 
+Veuillez consulter les informations ci-dessous:</p>
+
+<ul>
+    <li>Nom: </li>
+    <li>Prénom: </li>
+    <li>Date de début: <?= date('d-m-Y', strtotime($annonce->getDateDebut())) ?></li>
+    <li>Date de fin: <?= date('d-m-Y', strtotime($annonce->getDateFin())) ?></li>
+    <li>Adresse: </li>
+</ul>
+
+<p>Le montant de la réservation est de: </p>
+
+<p>Si vous avez des questions ou des modifications à apporter à votre réservation, 
+veuillez nous contacter au plus tôt.</p> 
+<p>Nous vous souhaitons un agréable séjour chez Chouchou!</p
