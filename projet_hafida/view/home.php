1fb173323059d5b42ccd4f3248094ab9228317de
Hafida3412 <lounsy81@gmail.com>
2024-07-13 23:19:46 +0200

    création du welcome.css

--- projet_hafida/view/home.php
+++ projet_hafida/view/home.php
@@ -1,11 +1,9 @@
-<h1>BIENVENUE CHEZ MON CHAT</h1>
+<h1 class="welcome-title">BIENVENUE CHEZ MON CHAT</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<p class="welcome-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-<?php
-
-    ?>
-    <a href="index.php?ctrl=security&action=login">Se connecter</a>
-    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
+<p class="welcome-links">
+  <a href="index.php?ctrl=security&action=login">Se connecter</a>
+  <a href="index.php?ctrl=security&action=register">S'inscrire</a>
 </p>
