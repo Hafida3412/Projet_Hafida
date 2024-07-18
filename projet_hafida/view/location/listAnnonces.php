5c84577775dbcb4b010c7ac5c2508f7e16776cd0
Hafida3412 <lounsy81@gmail.com>
2024-07-18 12:06:44 +0200

    modification de la vue listAnnonces

--- projet_hafida/view/location/listAnnonces.php
+++ projet_hafida/view/location/listAnnonces.php
@@ -14,14 +14,14 @@ foreach($annonces as $annonce){
     "<br> Nb de chats: ".$annonce->getNbChat()."<br>
     Date de début: ".(date('d-m-Y ', strtotime($annonce->getDateDebut())))."<br>
     Date de fin: ".(date('d-m-Y ', strtotime($annonce->getDateFin()))).
-    "<br> Description: ".$annonce->getDescription()."<br> Ville: ".$annonce->getLogement()->getVille()."</p><br>";
+    "<br> Description: ".$annonce->getDescription()."<br> Ville: ".$annonce->getLogement()->getVille()."<br>"."<br>";
 
     // si l'utilisateur est connecté
     if(App\Session::getUtilisateur()) {
         // si l'id de l'utilisateur de l'annonce = id de l'utilisateur connecté 
         if(App\Session::getUtilisateur()->getId() == $annonce->getUtilisateur()->getId()) {
         ?>
-            <a href="index.php?ctrl=location&action=supprimerAnnonce&id=<?=$annonce->getId() ?>"><button>Supprimer</button></a><br>
+            <a href="index.php?ctrl=location&action=supprimerAnnonce&id=<?=$annonce->getId() ?>"><button>Supprimer</button></a><br><br></p>
             <?php   
         }
         
