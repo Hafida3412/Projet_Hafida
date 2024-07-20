23dd59cf3092bb66b11e78fed343c7028cbf97ac
Hafida3412 <lounsy81@gmail.com>
2024-07-20 15:21:10 +0200

    rajout d'une image dans la vue register.php

--- projet_hafida/view/connexion/register.php
+++ projet_hafida/view/connexion/register.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
-
+<img src="public\img\chat.png" alt="regard d'un chat" style="width: 100%; height: auto; object-fit: contain;">
 <h1>S'inscrire</h1>
 
 <form action="index.php?ctrl=security&action=register" method="POST"><!--STRUCTURE DE L'URL POUR DECLENCHER UNE ACTION: INDEX.PHP?CTRL ACTION= METHOD= ID= -->
