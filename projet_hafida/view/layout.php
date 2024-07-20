6115e5b696e7dc2f4d80f9815da7801fdbdf5e03
Hafida3412 <lounsy81@gmail.com>
2024-07-20 14:44:12 +0200

    Modification des fichiers css

--- projet_hafida/view/layout.php
+++ projet_hafida/view/layout.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/layout.css">
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/welcome.css">
+        <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/register.css">
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/login.css">
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/listAnnonces.css">
         <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/ajoutAnnonces.css">
