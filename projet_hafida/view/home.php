6582aa2af3f767b2d1cc1be45fcda8d1f3979579
Hafida3412 <lounsy81@gmail.com>
2024-07-27 15:32:50 +0200

    Modification du texte de la vue home.php

--- projet_hafida/view/home.php
+++ projet_hafida/view/home.php
@@ -2,8 +2,14 @@
 <img src="public\img\chat.png" alt="regard d'un chat" style="width: 100%; height: auto; object-fit: contain;">
 <h1 class="welcome-title">BIENVENUE CHEZ MON CHAT</h1>
 
-<p class="welcome-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<p class="welcome-text">Sur notre site chez mon chat, vous pouvez trouver des 
+  propriétaires de félins qui cherchent quelqu'un pour s'occuper de leur animal 
+  pendant leur absence. 
+  Cela vous permet de profiter de vacances à moindre coût en échange de quelques 
+  attentions pour leur chat. 
+  N'hésitez pas à consulter les annonces disponibles et à contacter les propriétaires 
+  pour organiser cette garde temporaire. 
+  Bonnes vacances à vous !</p>
 
 <p class="welcome-links">
   <a href="index.php?ctrl=security&action=login">Se connecter</a>
