b8238c5186ed9489a8bf450e337637a502820d45
Hafida3412 <lounsy81@gmail.com>
2024-07-29 16:37:18 +0200

    Création de la vue Mentions Légales

---
+++ projet_hafida/view/location/mentionsLegales.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<h1>Mentions légales conformes au RGPD pour le site "Chez Mon Chat"</h1>
+    <ul>
+        <li>Responsable de traitement : [Nom de l'entreprise]</li>
+        <li>Siège social : [Adresse]</li>
+        <li>Numéro de téléphone : [Numéro de téléphone]</li>
+        <li>Adresse email : [Adresse email]</li>
+        <li>Numéro de TVA intracommunautaire : [Numéro de TVA]</li>
+        <li>Statut : [Société par actions simplifiée, société à responsabilité limitée, etc.]</li>
+        <li>Directeur de la publication : [Nom du directeur]</li>
+        <li>Hébergeur du site : [Nom de l'hébergeur]</li>
+        <li>Données collectées : Les données collectées sont celles renseignées par les utilisateurs lors de leur inscription sur le site (nom, prénom, adresse email, etc.).</li>
+        <li>Finalités du traitement des données : Les données collectées sont utilisées dans le but de permettre aux utilisateurs de créer un compte sur le site et de bénéficier des services offerts par celui-ci.</li>
+        <li>Durée de conservation des données : Les données sont conservées pendant une durée maximale de [X] ans à compter de la dernière interaction de l'utilisateur avec le site.</li>
+        <li>Sécurité des données : Les données collectées sont stockées de manière sécurisée et ne sont pas communiquées à des tiers sans le consentement de l'utilisateur.</li>
+        <li>Droits des utilisateurs : Les utilisateurs disposent d'un droit d'accès, de rectification, d'effacement et d'opposition aux données les concernant. Ils peuvent exercer ces droits en contactant le responsable de traitement à l'adresse [adresse email].</li>
+        <li>Consentement : En utilisant le site "Chez Mon Chat", les utilisateurs consentent à la collecte et au traitement de leurs données personnelles conformément aux dispositions légales en vigueur.</li>
+        <li>Modification des mentions légales : Les présentes mentions légales peuvent être modifiées à tout moment sans préavis. Il est recommandé aux utilisateurs de consulter régulièrement cette page pour prendre connaissance des éventuelles mises à jour.</li>
+    </ul>
