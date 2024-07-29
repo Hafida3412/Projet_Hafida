602716caa26378d6433715ec203bb01ded6e484c
Hafida3412 <lounsy81@gmail.com>
2024-07-29 16:57:41 +0200

    création de la vue reglement.php

---
+++ projet_hafida/view/location/reglement.php
@@ -0,0 +1,40 @@
+<?php 
+?>
+<h1>Réglement du site</h1>
+<ul> 
+    <li>Collecte et traitement des données personnelles:<br>
+    Nous collectons uniquement les données personnelles nécessaires pour fournir et 
+    améliorer nos services. Ces données peuvent inclure votre nom, adresse e-mail, 
+    adresse postale, numéro de téléphone, etc. Nous ne collectons pas de données 
+    sensibles sans votre consentement explicite.  
+    </li>
+
+    <li>Utilisation des données personnelles:<br>
+    Les données personnelles que nous collectons sont utilisées pour vous fournir 
+    des services personnalisés, répondre à vos demandes, gérer votre compte utilisateur,
+    etc. Nous ne vendrons, ne louerons ni ne partagerons vos données personnelles avec 
+    des tiers sans votre consentement préalable.   
+    </li> 
+
+    <li>Sécurité des données:<br>
+    Nous prenons des mesures de sécurité appropriées pour protéger vos données 
+    personnelles contre tout accès non autorisé, divulgation ou destruction. Vos données
+    sont stockées de manière sécurisée et ne sont accessibles que par un nombre limité 
+    de personnes autorisées.
+    </li> 
+
+    <li>Cookies:<br>
+    Nous utilisons des cookies pour améliorer votre expérience de navigation sur notre 
+    site. Les cookies sont de petits fichiers stockés sur votre appareil qui nous aident
+     à vous reconnaître et à personnaliser votre expérience. Vous pouvez modifier les 
+     paramètres de votre navigateur pour refuser les cookies, mais cela peut affecter 
+     certaines fonctionnalités de notre site.
+    </li> 
+
+    <li>Consentement et droit de retrait:<br>
+    En utilisant notre site, vous consentez au traitement de vos données personnelles 
+    conformément à cette politique de confidentialité. Vous avez le droit de retirer 
+    votre consentement à tout moment en nous contactant. Vous avez également le droit 
+    d'accéder, de rectifier ou de supprimer vos données personnelles.
+    </li> 
+</ul>
