32e911d6904341983ae498ff146f427cbdb5c485
Hafida3412 <lounsy81@gmail.com>
2024-07-29 19:40:06 +0200

    déplacement du popup cookie dans la vue login.php

--- projet_hafida/view/connexion/login.php
+++ projet_hafida/view/connexion/login.php
@@ -12,3 +12,10 @@
 <input type="password" name="password" id="password" value="aaaaa"><br>
 <input type="submit" name="submitLogin" value="Se connecter">
 </form>
+
+<!--CREATION DU POP UP COOKIE-->
+<div id="cookie-popup">
+  <p>Notre site utilise des cookies pour améliorer votre expérience. En continuant à naviguer sur notre site, vous acceptez l'utilisation de ces cookies.</p>
+      <button id="accept-cookies" onclick="acceptCookies()">J'accepte</button>
+      <button id="refuse-cookies" onclick="refuseCookies()">Je refuse</button>
+</div>
--- projet_hafida/view/home.php
+++ projet_hafida/view/home.php
@@ -16,9 +16,3 @@
   <a href="index.php?ctrl=security&action=register">S'inscrire</a>
   </p>
 <br>
-<!--CREATION DU POP UP COOKIE-->
-<div id="cookie-popup">
-  <p>Notre site utilise des cookies pour améliorer votre expérience. En continuant à naviguer sur notre site, vous acceptez l'utilisation de ces cookies.</p>
-      <button id="accept-cookies" onclick="acceptCookies()">J'accepte</button>
-      <button id="refuse-cookies" onclick="refuseCookies()">Je refuse</button>
-</div>
