e2997e492371460a6d6f529f0337ea70af124373
Hafida3412 <lounsy81@gmail.com>
2024-08-02 09:07:32 +0200

    création de listeLogementsUtilisateur.css

--- projet_hafida/view/location/listeLogementsUtilisateur.php
+++ projet_hafida/view/location/listeLogementsUtilisateur.php
@@ -1,13 +1,17 @@
 <?php
- $logements = $result["data"]['logements']; 
- ?>
+$logements = $result["data"]['logements']; 
+?>
 <?php
 foreach($logements as $logement){
-    // Afficher les détails du logement, par exemple :
-    echo "ID : ".$logement->getId()."<br>";
-    echo "Nombre de chambres : ".$logement->getNbChambre()."<br>";
-    echo "Rue : ".$logement->getRue()."<br>";
-    echo "CP : ".$logement->getCP()."<br>";
-    echo "Ville : ".$logement->getVille()."<br>";
-    echo "Image : <img src='".$logement->getImage()."' alt='Image du logement'><br><br>";
+    // Afficher les détails du logement
+    echo "<div class='logement-info'>";
+    echo "<p>ID : ".$logement->getId()."</p>";
+    echo "<p>Nombre de chambres : ".$logement->getNbChambre()."</p>";
+    echo "<p>Rue : ".$logement->getRue()."</p>";
+    echo "<p>CP : ".$logement->getCP()."</p>";
+    echo "<p>Ville : ".$logement->getVille()."</p>";
+    echo "<div class='logement-image'><img src='".$logement->getImage()."' alt='Image du logement'></div>";
+    echo "</div>";
 }
+?> 
+
