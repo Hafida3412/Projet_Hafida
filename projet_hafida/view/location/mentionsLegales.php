c97807a333bed63d2b77bd23fc5ded6e5033c249
Hafida3412 <lounsy81@gmail.com>
2024-08-19 11:13:26 +0200

    modification de la vue mentions légales

--- projet_hafida/view/location/mentionsLegales.php
+++ projet_hafida/view/location/mentionsLegales.php
@@ -14,7 +14,7 @@
         <li>Hébergeur du site : [Nom de l'hébergeur]</li>
         <li>Données collectées : Les données collectées sont celles renseignées par les utilisateurs lors de leur inscription sur le site (nom, prénom, adresse email, etc.).</li>
         <li>Finalités du traitement des données : Les données collectées sont utilisées dans le but de permettre aux utilisateurs de créer un compte sur le site et de bénéficier des services offerts par celui-ci.</li>
-        <li>Durée de conservation des données : Les données sont conservées pendant une durée maximale de [X] ans à compter de la dernière interaction de l'utilisateur avec le site.</li>
+        <li>Durée de conservation des données : Les données sont conservées pendant une durée maximale de 3 ans à compter de la dernière interaction de l'utilisateur avec le site.</li>
         <li>Sécurité des données : Les données collectées sont stockées de manière sécurisée et ne sont pas communiquées à des tiers sans le consentement de l'utilisateur.</li>
         <li>Droits des utilisateurs : Les utilisateurs disposent d'un droit d'accès, de rectification, d'effacement et d'opposition aux données les concernant. Ils peuvent exercer ces droits en contactant le responsable de traitement à l'adresse [adresse email].</li>
         <li>Consentement : En utilisant le site "Chez Mon Chat", les utilisateurs consentent à la collecte et au traitement de leurs données personnelles conformément aux dispositions légales en vigueur.</li>
