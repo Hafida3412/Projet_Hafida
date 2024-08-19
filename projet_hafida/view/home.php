547be9ee4e28be8834075bae3d8ba38728e50997
Hafida3412 <lounsy81@gmail.com>
2024-08-19 21:02:10 +0200

    modification de la page d'accueil

--- projet_hafida/view/home.php
+++ projet_hafida/view/home.php
@@ -1,18 +1,24 @@
+<div class="parallax">
+  <div class="parallax-bg">
+  <img src="public\img\chat.png" alt="regard d'un chat" style="width: 100%; height: auto; object-fit: contain;">
+  </div>
+  <div class="content">
+    
+    <h1 class="welcome-title">BIENVENUE CHEZ MON CHAT</h1>
+    <p class="welcome-text">Sur notre site chez mon chat, vous pouvez trouver des 
+      propriétaires de félins qui cherchent quelqu'un pour s'occuper de leur animal 
+      pendant leur absence. 
+      Cela vous permet de profiter de vacances à moindre coût en échange de quelques 
+      attentions pour leur chat. 
+      N'hésitez pas à consulter les annonces disponibles et à contacter les propriétaires 
+      pour organiser cette garde temporaire. 
+      Bonnes vacances à vous !</p>
+    <br>
+  </div>
+</div>
 
-<img src="public\img\chat.png" alt="regard d'un chat" style="width: 100%; height: auto; object-fit: contain;">
-<h1 class="welcome-title">BIENVENUE CHEZ MON CHAT</h1>
-
-<p class="welcome-text">Sur notre site chez mon chat, vous pouvez trouver des 
-  propriétaires de félins qui cherchent quelqu'un pour s'occuper de leur animal 
-  pendant leur absence. 
-  Cela vous permet de profiter de vacances à moindre coût en échange de quelques 
-  attentions pour leur chat. 
-  N'hésitez pas à consulter les annonces disponibles et à contacter les propriétaires 
-  pour organiser cette garde temporaire. 
-  Bonnes vacances à vous !</p>
-  <br>
 <p class="welcome-links">
   <a href="index.php?ctrl=security&action=login">Se connecter</a>
   <a href="index.php?ctrl=security&action=register">S'inscrire</a>
-  </p>
+</p>
 <br>
