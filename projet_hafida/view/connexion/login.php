20ca61b4e2f13f0067d9a72f0044aed4ba434221
Hafida3412 <lounsy81@gmail.com>
2024-08-22 09:54:42 +0200

    modification de la vue login

--- projet_hafida/view/connexion/login.php
+++ projet_hafida/view/connexion/login.php
@@ -6,10 +6,10 @@
 
 <form action="index.php?ctrl=security&action=login"method="post">
 <label for="email">Email</label>
-<input type="email" name="email" id="email" value="micka@exemple.com"><br>
+<input type="email" name="email" id="email" value="lea@gmail.com"><br>
 
 <label for="password">Mot de passe</label>
-<input type="password" name="password" id="password" value="aaaaa"><br>
+<input type="password" name="password" id="password" value="helloworld"><br>
 
 <input type="submit" name="submitLogin" value="Se connecter">
 </form>
