6d5e58e9b9871c77616786e1b252aadfd0acced3
Hafida3412 <lounsy81@gmail.com>
2024-08-25 16:10:53 +0200

    Rajout de commentaires à ReservationsController

--- projet_hafida/controller/ReservationsController.php
+++ projet_hafida/controller/ReservationsController.php
@@ -69,6 +69,7 @@ class ReservationsController extends AbstractController implements ControllerInt
         ];   
     }
 
+    //FONCTION CONFIRMATION
     public function confirmation(){
         
                 return [
