fc8535e675453e136134f642876cb3d1954875b5
Hafida3412 <lounsy81@gmail.com>
2024-08-29 14:11:10 +0200

    rajout de commentaire à l'adminController

--- projet_hafida/controller/AdminController.php
+++ projet_hafida/controller/AdminController.php
@@ -12,18 +12,17 @@ class AdminController extends AbstractController{
 //FONCTION POUR LISTER TOUS LES UTILISATEURS
 
 public function listUtilisateurs() {
-    // Vérifier si l'utilisateur est connecté et s'il a le rôle "ROLE_ADMIN"
+    // On vérifie si l'utilisateur est connecté et s'il a le rôle "ROLE_ADMIN"
     if (!Session::getUtilisateur() || !Session::getUtilisateur()->hasRole("ROLE_ADMIN")) {
         header('Location: index.php?ctrl=security&action=login'); // Rediriger vers la page de connexion si l'utilisateur n'est pas admin
         exit;
     }
 
-    // Récupérer tous les utilisateurs
+    // On récupère tous les utilisateurs
     $utilisateurManager = new UtilisateurManager();
     $utilisateurs = $utilisateurManager->findAll();
 
-
-    // Retourner la vue et les données
+    // On retourne la vue et les données
     return [
         'view' => VIEW_DIR . 'admin/listUtilisateurs.php',
         "meta_description" => "Liste des Utilisateurs",
