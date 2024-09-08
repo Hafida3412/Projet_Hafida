eededaa3ae97246e427dd1ce6a7dae9a6e658ee0
Hafida3412 <lounsy81@gmail.com>
2024-09-08 19:53:51 +0200

    animation des images uploadées dans detailsAnnonce

--- projet_hafida/public/js/script.js
+++ projet_hafida/public/js/script.js
@@ -6,4 +6,25 @@ function acceptCookies() {
   function refuseCookies() {
     document.getElementById("cookie-popup").style.display = "none";
   }
-  
+
+
+  let currentIndex = 0;
+  const images = document.querySelectorAll('.images img');
+  const totalImages = images.length;
+
+  function showImage(index) {
+      images.forEach((img, i) => {
+          img.classList.remove('active'); // Masquer toutes les images
+          if (i === index) {
+              img.classList.add('active'); // Affiche l'image active
+          }
+      });
+  }
+
+  function nextImage() {
+      currentIndex = (currentIndex + 1) % totalImages; // passe à l'image suivante
+      showImage(currentIndex);
+  }
+
+  setInterval(nextImage, 3000); // Change d'image toutes les 3 secondes
+  showImage(currentIndex); // Affiche la première image
