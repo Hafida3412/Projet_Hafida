4e54e5396f1927dfedbf840f036d222ade340a25
Hafida3412 <lounsy81@gmail.com>
2024-09-08 20:08:08 +0200

    rajout de commentaires à script.js

--- projet_hafida/public/js/script.js
+++ projet_hafida/public/js/script.js
@@ -8,23 +8,31 @@ function acceptCookies() {
   }
 
 
+  //ANIMATION DES IMAGES UPLOADEES DANS DETAILSANNONCE
+  // Index actuel pour l'image affichée
   let currentIndex = 0;
+  // Sélectionne tous les éléments <img> dans la classe 'images'
   const images = document.querySelectorAll('.images img');
+  // Nombre total d'images
   const totalImages = images.length;
 
+
+  // Fonction pour afficher l'image correspondant à l'index donné
   function showImage(index) {
       images.forEach((img, i) => {
-          img.classList.remove('active'); // Masquer toutes les images
+          img.classList.remove('active'); // masque toutes les images en retirant la classe 'active'
           if (i === index) {
-              img.classList.add('active'); // Affiche l'image active
+              img.classList.add('active'); // ajoute la classe 'active' uniquement à l'image correspondante à l'index
           }
       });
   }
 
+
+  // Fonction pour passer à l'image suivante
   function nextImage() {
-      currentIndex = (currentIndex + 1) % totalImages; // passe à l'image suivante
-      showImage(currentIndex);
+      currentIndex = (currentIndex + 1) % totalImages; //  // Passe à l'image suivante en réinitialisant à 0 si à la fin
+      showImage(currentIndex);// Affiche l'image active mise à jour
   }
 
   setInterval(nextImage, 3000); // Change d'image toutes les 3 secondes
-  showImage(currentIndex); // Affiche la première image
+  showImage(currentIndex); // Affiche la première image au chargement de la page
