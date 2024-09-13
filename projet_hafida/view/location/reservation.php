4a2ffe0aebf24a5cd7d77318367dd9970e6e25d2
Hafida3412 <lounsy81@gmail.com>
2024-09-13 16:06:52 +0200

    Rajout de commentaires à la vue reservation.php

--- projet_hafida/view/location/reservation.php
+++ projet_hafida/view/location/reservation.php
@@ -1,23 +1,27 @@
 <?php  
+ // Récupère l'ID de l'annonce à partir des données de réponse
   $annonceId = $result["data"]['annonceId']; 
 ?>
 <div class="bg_reservation">
 <h1>Réservation</h1>
 
+<!-- Formulaire de réservation avec méthode POST -->
 <form action="index.php?ctrl=reservations&action=reservation&annonceId=<?= $annonceId?>" method="POST">
 
-
-
+     <!-- Champ pour le numéro de téléphone -->
     <label for="numeroTelephone">Numéro de téléphone</label>
     <input type="text" name="numeroTelephone" id="numeroTelephone"><br>
 
+    <!-- Champ pour le nombre d'adultes -->
     <label for="nbAdultes">Nb d'adultes</label>
     <input type="number" name="nbAdultes"><br>
 
+    <!-- Champ pour le nombre d'enfants -->
     <label for="nbEnfants">Nb d'enfants</label>
     <input type="number" name="nbEnfants"><br>
 
     <div class="paiement">
+        <!-- Section pour choisir le mode de paiement -->
         <label for="paiement">Mode de Paiement:</label>
         <input type="radio" id="cb" name="paiement" value="cb">
         <label for="cb">Carte Bancaire (Visa, Mastercard)</label>
@@ -25,11 +29,11 @@
         <label for="paypal">PayPal</label>
     </div>
     
+    <!-- Champ pour les demandes particulières -->
     <label for="question">Avez-vous une demande particulière?</label>
     <textarea name="question" rows="4" cols="50"></textarea><br>
     
+    <!-- Bouton de soumission pour la réservation -->
     <input type="submit" name="submitReservation" value="Réserver">
 </form>
-
-
 </div>
