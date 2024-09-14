6f542343cda62ec5862cedf8d05b3bbfa4eb9bc6
Hafida3412 <lounsy81@gmail.com>
2024-09-15 01:45:48 +0200

    modification du css de la vue listLogementsUtilisateur

--- projet_hafida/view/location/listeLogementsUtilisateur.php
+++ projet_hafida/view/location/listeLogementsUtilisateur.php
@@ -1,22 +1,28 @@
 <?php
 $logements = $result["data"]['logements']; 
 ?>
-<?php
-// Vérifiez si le tableau de logements est vide
-if (empty($logements)) {
-    echo "<p>Aucun logement disponible.</p>";
-} else {
-foreach($logements as $logement){
-    // Afficher les détails du logement
-    echo "<div class='logement-info'>";
-    echo "<p>ID : ".$logement->getId()."</p>";
-    echo "<p>Nombre de chambres : ".$logement->getNbChambre()."</p>";
-    echo "<p>Rue : ".$logement->getRue()."</p>";
-    echo "<p>CP : ".$logement->getCP()."</p>";
-    echo "<p>Ville : ".$logement->getVille()."</p>";
-    echo "<div class='logement-image'><img src='".$logement->getImage()."' alt='Image du logement'></div>";
-    echo "</div>";
-}
-}
-?> 
+<h1>Mes logements</h1>
+
+<div class="details-container">
+    <div class="bg_details">
+
+    <?php
+    // Vérifiez si le tableau de logements est vide
+    if (empty($logements)) {
+        echo "<p>Aucun logement disponible.</p>";
+    } else {
+        foreach ($logements as $logement) {
+            // Afficher les détails du logement
+            echo "<div class='annonce-info'>";
+            echo "<h2>Référence : " . htmlspecialchars($logement->getId()) . "</h2>";
+            echo "<p><strong>Type de logement:</strong> " . htmlspecialchars($logement->getTypeLogement()->getNomType()) . "</p>";
+            echo "<p><strong>Nombre de chambres:</strong> " . htmlspecialchars($logement->getNbChambre()) . "</p>";
+            echo "<p><strong>Ville:</strong> " . htmlspecialchars($logement->getVille()) . "</p>";
+            echo "<div class='logement-image'><img src='" . htmlspecialchars($logement->getImage()) . "' alt='Image du logement' class='annonce-info-img'></div>";
+            echo "</div>";
+        }
+    }
+    ?>
+    </div>
+</div>
 
