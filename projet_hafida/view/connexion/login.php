30bbbb2c25fd488a37f5a6ed947304ec4b279ad2
Hafida3412 <lounsy81@gmail.com>
2024-09-17 11:21:10 +0200

    création du recaptcha dans la fonction login

--- projet_hafida/view/connexion/login.php
+++ projet_hafida/view/connexion/login.php
@@ -14,8 +14,10 @@
 <label for="password">Mot de passe</label>
 <input type="password" name="password" id="password" value="AZERTY12345"><br>
 
+<div class="g-recaptcha" data-sitekey="6LeUhkYqAAAAALeAYNXSofNNSW3Fw9vzIRzl4Ngw" data-action="LOGIN"></div>
+<br/>
 <input type="submit" name="submitLogin" value="Se connecter">
-
+<script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
 <!-- Lien vers la vue forgotPassword -->
 <p><a href="index.php?ctrl=security&action=forgotPassword">Mot de passe oublié ?</a></p>
 
