dfd1b92df24d75699541987c26ad96312f090474
Hafida3412 <lounsy81@gmail.com>
2024-09-21 13:24:17 +0200

    rajout de commentaires à l'entité TypeLogement

--- projet_hafida/model/entities/TypeLogement.php
+++ projet_hafida/model/entities/TypeLogement.php
@@ -7,25 +7,31 @@ use App\Entity;
     En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
 */
 
+// Déclaration de la classe TypeLogement qui est finale et hérite de la classe Entity
 final class TypeLogement extends Entity{
 
-    private $id;
-    private $nomType;
+    // Propriétés de la classe
+    private $id;          // Identifiant unique du type de logement
+    private $nomType;     // Nom du type de logement
 
+    // Constructeur qui initialise l'objet avec les données fournies
     public function __construct($data){         
-        $this->hydrate($data);     
+        $this->hydrate($data);  // Hydrate l'objet avec les données   
     }
 
+    // Méthode pour récupérer l'identifiant du type de logement
     public function getId()
     {
         return $this->id;
     }
 
+    // Méthode pour définir l'identifiant du type de logement
     public function setId($id)
     {
-        $this->id = $id;
+        $this->id = $id; // Affecte la valeur à l'identifiant
 
-        return $this;
+
+        return $this; // Permet le chaînage des méthodes
     }
 
     
