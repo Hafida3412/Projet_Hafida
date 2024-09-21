09fdfd8d20523f8a9da5118b217257b1b5ff0133
Hafida3412 <lounsy81@gmail.com>
2024-09-21 12:02:59 +0200

    Correction et rajout de commentaires de ReservationsController

--- projet_hafida/controller/ReservationsController.php
+++ projet_hafida/controller/ReservationsController.php
@@ -8,55 +8,56 @@ use Model\Managers\AnnonceManager;
 use Model\Managers\ReserverManager;
 
 class ReservationsController extends AbstractController implements ControllerInterface{
+
+//FONCTION DE RESERVATION D UNE ANNONCE
+
     public function reservation(){ // On vérifie que l'utilisateur est connecté
          if (!Session::getUtilisateur()) {
              // Redirection vers la page de connexion si l'utilisateur n'est pas connecté 
              $this->redirectTo("connexion", "login"); return; }
 
  
-// On récupère l'ID de l'annonce via GET
-$annonceId = filter_input(INPUT_GET, 'annonceId', FILTER_VALIDATE_INT);
-// var_dump($annonceId); die;
-
+        // On récupère l'ID de l'annonce via GET
+            $annonceId = filter_input(INPUT_GET, 'annonceId', FILTER_VALIDATE_INT);
+        // var_dump($annonceId); die;
  
-if ($annonceId === null) {
-
-    // Redirection ou gestion d'erreur si l'ID de l'annonce n'est pas fourni
-Session::addFlash("error", "Aucun ID d'annonce n'a été fourni.");
-$this->redirectTo("location", "index");
-return;
-}
-//var_dump($annonceId);die;
- if(isset($_POST["submitReservation"])){
+            if ($annonceId === null) {
+        // Redirection ou gestion d'erreur si l'ID de l'annonce n'est pas fourni
+            Session::addFlash("error", "Aucun ID d'annonce n'a été fourni.");
+            $this->redirectTo("location", "index");
+            return;
+        }
+        //var_dump($annonceId);die;
+            if(isset($_POST["submitReservation"])){
  
         // On vérifie si l'annonce est déjà réservée
-     $annonceManager = new AnnonceManager();
-     $estValide = $annonceManager->isAnnonceValide($annonceId);
-    //var_dump($estValide);die;
+            $annonceManager = new AnnonceManager();
+            $estValide = $annonceManager->isAnnonceValide($annonceId);
+        //var_dump($estValide);die;
     
-   if($estValide){
-    // Message d'erreur si l'annonce est déjà réservée
-        Session::addFlash("error", "Cette annonce est déjà réservée.");
-        $this->redirectTo("location", "index");
-        return;
-    }
+             if($estValide){
+        // Message d'erreur si l'annonce est déjà réservée
+             Session::addFlash("error", "Cette annonce est déjà réservée.");
+             $this->redirectTo("location", "index");
+            return;
+        }
 
-    // On filtre les données du formulaire
-    $numeroTelephone = filter_input(INPUT_POST, "numeroTelephone", FILTER_SANITIZE_SPECIAL_CHARS);
-    $nbAdultes = filter_input(INPUT_POST, "nbAdultes", FILTER_VALIDATE_INT);
-    $nbEnfants = filter_input(INPUT_POST, "nbEnfants", FILTER_VALIDATE_INT);
-    $paiement = filter_input(INPUT_POST, "paiement", FILTER_SANITIZE_SPECIAL_CHARS);
-    $question = filter_input(INPUT_POST, "question", FILTER_SANITIZE_SPECIAL_CHARS);
-    //$annonceId = filter_input(INPUT_GET, "annonceId", FILTER_VALIDATE_INT);
-    //var_dump($_GET['id']);die;
-    $valide = 1; // Réservation validée
-   //var_dump($numeroTelephone, $nbAdultes, $nbEnfants, $paiement, $question);die;
+        // On filtre les données du formulaire
+            $numeroTelephone = filter_input(INPUT_POST, "numeroTelephone", FILTER_SANITIZE_SPECIAL_CHARS);
+            $nbAdultes = filter_input(INPUT_POST, "nbAdultes", FILTER_VALIDATE_INT);
+            $nbEnfants = filter_input(INPUT_POST, "nbEnfants", FILTER_VALIDATE_INT);
+            $paiement = filter_input(INPUT_POST, "paiement", FILTER_SANITIZE_SPECIAL_CHARS);
+            $question = filter_input(INPUT_POST, "question", FILTER_SANITIZE_SPECIAL_CHARS);
+        //$annonceId = filter_input(INPUT_GET, "annonceId", FILTER_VALIDATE_INT);
+        //var_dump($_GET['id']);die;
+            $valide = 1; // Réservation validée
+        //var_dump($numeroTelephone, $nbAdultes, $nbEnfants, $paiement, $question);die;
    
-   // Vérification des données
-  if ($numeroTelephone && $nbAdultes !== false && $nbEnfants !== false && $paiement)  {
+        // Vérification des données
+            if ($numeroTelephone && $nbAdultes !== false && $nbEnfants !== false && $paiement)  {
         //Enregistrement des informations de la réservation dans la base de données
-        $reserverManager = new ReserverManager();
-        $result= $reserverManager->add([
+             $reserverManager = new ReserverManager();
+             $result= $reserverManager->add([
             "numeroTelephone" => $numeroTelephone,
             "nbAdultes" => $nbAdultes,
             "nbEnfants" => $nbEnfants,
@@ -65,36 +66,37 @@ return;
             "valide" => $valide,
             "annonce_id" => $annonceId,
             "utilisateur_id" => Session::getUtilisateur()->getId()
-        ]);
-       //var_dump($result);die;
+            ]);
+        //var_dump($result);die;
+
         // Mise à jour du statut de l'annonce pour indiquer qu'elle est fermée
             $annonceManager->updateDisponibilite($annonceId);
-            // Ajout d'un message de confirmation pour l'utilisateur
+        // Ajout d'un message de confirmation pour l'utilisateur
             Session::addFlash("success", "Votre réservation a été effectuée avec succès.");
 
-            // Redirection vers la confirmation de réservation
+        // Redirection vers la confirmation de réservation
             $this->redirectTo("location", "confirmation");
             return;
-  }
+    }
 }
 
-return [
-    "view" => VIEW_DIR . "location/reservation.php",
-    "meta_description" => "Formulaire de réservation",
-    "data" => [ 
-        "annonceId" => $annonceId 
-    ]
-];   
-   }
+            return [
+            "view" => VIEW_DIR . "location/reservation.php",
+            "meta_description" => "Formulaire de réservation",
+            "data" => [ 
+            "annonceId" => $annonceId 
+        ]
+    ];   
+}
 
  
-//FONCTION CONFIRMATION
- public function confirmation(){
+//FONCTION DE CONFIRMATION
 
-            return [
-                "view" => VIEW_DIR . "location/confirmation.php",
-                "meta_description" => "Confirmation de réservation",
-            ];
+    public function confirmation(){
+        return [
+        "view" => VIEW_DIR . "location/confirmation.php",
+        "meta_description" => "Confirmation de réservation",
+        ];
     }
 }
     
