c313f8d0acd1456e224024f75ca285b1dc51b760
Hafida3412 <lounsy81@gmail.com>
2024-09-29 22:56:59 +0200

    Rajout de commentaires à HomeController

--- projet_hafida/controller/HomeController.php
+++ projet_hafida/controller/HomeController.php
@@ -1,31 +1,37 @@
 <?php
-namespace Controller;
+namespace Controller; // Déclaration du namespace pour organiser le cod
 
-use App\AbstractController;
-use App\ControllerInterface;
-use Model\Managers\UtilisateurManager;
+use App\AbstractController; // Importation de la classe AbstractController
+use App\ControllerInterface; // Importation de l'interface ControllerInterface
+use Model\Managers\UtilisateurManager; /* Importation du gestionnaire UtilisateurManager qui interagit 
+avec les utilisateurs*/
 
-
+// Déclaration de la classe HomeController qui étend AbstractController et implémente ControllerInterface
 class HomeController extends AbstractController implements ControllerInterface {
 
+    // Méthode principale du controller, généralement utilisée pour afficher la page d'accueil
     public function index(){
         return [
-            "view" => VIEW_DIR."home.php",
-            "meta_description" => "Page d'accueil"
+            "view" => VIEW_DIR."home.php", // Chemin de la vue à afficher
+            "meta_description" => "Page d'accueil" // Description pour le SEO
         ];
     }
-        
+    
+    //Méthode pour afficher la liste des utilisateurs
     public function users(){
+        // Restriction d'accès pour s'assurer que l'utilisateur a le rôle approprié
         $this->restrictTo("ROLE_USER");
 
+         // Création d'une instance du gestionnaire d'utilisateurs
         $manager = new UtilisateurManager();
+        // Récupération de tous les utilisateurs triés par date d'inscription (DESC pour décroissant)
         $users = $manager->findAll(['register_date', 'DESC']);
 
         return [
-            "view" => VIEW_DIR."security/users.php",
-            "meta_description" => "Liste des utilisateurs",
+            "view" => VIEW_DIR."security/users.php", // Chemin de la vue à afficher pour la liste des utilisateurs
+            "meta_description" => "Liste des utilisateurs", // Description pour le SEO
             "data" => [ 
-                "utilisateurs" => $users 
+                "utilisateurs" => $users // Données des utilisateurs à passer à la vue
             ]
         ];
     }
