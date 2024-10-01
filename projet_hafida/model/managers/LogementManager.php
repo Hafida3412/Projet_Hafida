230d1481c53faa23ecca43a9c77604ee42752f33
Hafida3412 <lounsy81@gmail.com>
2024-10-01 23:44:22 +0200

    rajout de commentaires à LogementManager

--- projet_hafida/model/managers/LogementManager.php
+++ projet_hafida/model/managers/LogementManager.php
@@ -8,22 +8,25 @@ use App\DAO;
 class LogementManager extends Manager{
 
     // on indique la classe POO et la table correspondante en BDD pour le manager concerné
-    protected $className = "Model\Entities\Logement";
-    protected $tableName = "logement";
+    protected $className = "Model\Entities\Logement";// Classe qui représente un logement
+    protected $tableName = "logement";// Nom de la table dans la base de données
 
     public function __construct(){
+         // Appel du constructeur parent pour établir la connexion à la base de données
         parent::connect();
     }
   
-    // requete qui récupère la liste des logements de l'utilisateur connecté
+    // Méthode qui récupère la liste des logements pour un utilisateur spécifique, identifié par son ID
     public function listLogementsByUser($id){
+        // Requête SQL pour sélectionner tous les logements associés à un utilisateur donné
         $sql = "SELECT * 
         FROM ".$this->tableName." 
-        WHERE utilisateur_id = :id";
+        WHERE utilisateur_id = :id"; // Condition pour filtrer par ID d'utilisateur
 
+         // Exécution de la requête et récupération des résultats sous forme de tableau d'objets de la classe correspondante
         return $this->getMultipleResults(
-            DAO::select($sql, ['id' => $id]), 
-            $this->className
+            DAO::select($sql, ['id' => $id]), // Exécution de la requête avec le paramètre de l'ID utilisateur
+            $this->className // Spécification de la classe à laquelle les résultats doivent être mappés
         );
 
     }
