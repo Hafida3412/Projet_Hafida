dbea55d0a8873e364455a1890df374cef4c07130
Hafida3412 <lounsy81@gmail.com>
2024-10-15 11:46:53 +0200

    Correction de reservationsController

--- projet_hafida/controller/ReservationsController.php
+++ projet_hafida/controller/ReservationsController.php
@@ -14,37 +14,40 @@ class ReservationsController extends AbstractController implements ControllerInt
 //METHODE DE RESERVATION D UNE ANNONCE
 
     public function reservation(){ // On vérifie que l'utilisateur est connecté
-         if (!Session::getUtilisateur()) {
-             // Redirection vers la page de connexion si l'utilisateur n'est pas connecté 
-             $this->redirectTo("connexion", "login"); 
-             
-        // On récupère l'identifiant de l'annonce à partir des paramètres GET de l'URL, qui doit être un nombre entier.
-            $annonceId = filter_input(INPUT_GET, 'annonceId', FILTER_VALIDATE_INT);
-        // var_dump($annonceId); die;
         
-        // On vérifie que l'ID de l'annonce est présent
-            if ($annonceId === null) {
+        if (!Session::getUtilisateur()) {
+            // Redirection vers la page de connexion si l'utilisateur n'est pas connecté 
+            $this->redirectTo("connexion", "login");
+        }
+        
+    // On récupère l'identifiant de l'annonce à partir des paramètres GET de l'URL, qui doit être un nombre entier.
+        $annonceId = filter_input(INPUT_GET, 'annonceId', FILTER_VALIDATE_INT);
+    // var_dump($annonceId); die;
+    
+    // On vérifie que l'ID de l'annonce est présent
+        if ($annonceId === null) {
         // Redirection ou gestion d'erreur si l'ID de l'annonce n'est pas fourni
             Session::addFlash("error", "Aucun ID d'annonce n'a été fourni.");
             $this->redirectTo("location", "index");
             return;
         }
-        //var_dump($annonceId);die;
+    
+    //var_dump($annonceId);die;
 
-        // Vérification si le formulaire de réservation a été soumis
-            if(isset($_POST["submitReservation"])){ //$_POST contient les données envoyées via la méthode POST à partir du formulaire de réservation
+    // Vérification si le formulaire de réservation a été soumis
+        if(isset($_POST["submitReservation"])){ //$_POST contient les données envoyées via la méthode POST à partir du formulaire de réservation
         // On instancie un gestionnaire d'annonces pour vérifier si l'annonce est déjà réservée ($estValide)
             $annonceManager = new AnnonceManager();//classe qui gère les opérations liées aux annonces
             $estValide = $annonceManager->isAnnonceValide($annonceId);//méthode qui interroge la BDD pour déterminer si l'annonce est déjà réservée
         //var_dump($estValide);die;
-        
+    
         // Si l'annonce est déjà réservée, on affiche un message d'erreur
-             if($estValide){
-        // Message d'erreur si l'annonce est déjà réservée
-             Session::addFlash("error", "Cette annonce est déjà réservée.");
-             $this->redirectTo("location", "index");
-            return;
-        }
+            if($estValide){
+            // Message d'erreur si l'annonce est déjà réservée
+                Session::addFlash("error", "Cette annonce est déjà réservée.");
+                $this->redirectTo("location", "index");
+                return;
+            }
 
         // On filtre et nettoie les données fournies par l'utilisateur dans le formulaire de réservation.
         // Chaque champ est filtré pour éviter des attaques XSS et assurer qu'il soit conforme au type attendu.
@@ -59,12 +62,12 @@ class ReservationsController extends AbstractController implements ControllerInt
         //var_dump($_GET['id']);die;
             $valide = 1; // Réservation validée
         //var_dump($numeroTelephone, $nbAdultes, $nbEnfants, $paiement, $question);die;
-   
-        // Vérification de la validité des données fournies et on s'assure que toutes les données nécessaires sont fournies.
-            if ($nom && $prenom && $numeroTelephone && $nbAdultes !== false && $nbAdultes > 0 && $nbEnfants !== false && $paiement)  {
+
+    // Vérification de la validité des données fournies et on s'assure que toutes les données nécessaires sont fournies.
+        if ($nom && $prenom && $numeroTelephone && $nbAdultes !== false && $nbAdultes > 0 && $nbEnfants !== false && $paiement)  {
         //Cela garantit que nbAdultes est supérieur à 0 et que nbEnfants est valide (0 ou plus).
         //Enregistrement des informations de la réservation dans la base de données via $ReserverManager
-             $reserverManager = new ReserverManager();// classe utilisée pour gérer les opérations liées à la réservation incluant l'ajout de données à la BDD
+            $reserverManager = new ReserverManager();// classe utilisée pour gérer les opérations liées à la réservation incluant l'ajout de données à la BDD
             /* $result=*/ $reserverManager->add([
             "nom" => $nom,   
             "prenom" => $prenom,
@@ -77,7 +80,7 @@ class ReservationsController extends AbstractController implements ControllerInt
             "annonce_id" => $annonceId,
             "utilisateur_id" => Session::getUtilisateur()->getId()
             ]);
-        //var_dump($result);die;
+        // var_dump($result);die;
 
         // Mise à jour du statut de l'annonce pour indiquer qu'elle est réservée
             $annonceManager->updateDisponibilite($annonceId);
@@ -87,21 +90,22 @@ class ReservationsController extends AbstractController implements ControllerInt
         // Redirection vers la confirmation de réservation
             $this->redirectTo("reservations", "confirmation");
             return;
-    }
-}
-        // Si le formulaire n'a pas été soumis, la méthode prépare la vue de réservation, 
-        //qui affichera le formulaire correspondant à l'annonce sélectionnée.
-            return [
+        }
+        }
+    // Si le formulaire n'a pas été soumis, la méthode prépare la vue de réservation, 
+    //qui affichera le formulaire correspondant à l'annonce sélectionnée.
+
+        return [
             "view" => VIEW_DIR . "location/reservation.php",
             "meta" => "Formulaire de réservation",
             "title" => "Réservation d'annonce",
             "data" => [ 
             "annonceId" => $annonceId 
-        ]
-    ];   
+            ]
+        ];   
 }
 
-}
+
 //FONCTION DE CONFIRMATION
 
     public function confirmation(){
