76c06dac94a612f106be45f2401a0c33d29a6604
Hafida3412 <lounsy81@gmail.com>
2024-10-15 23:03:17 +0200

    Correction responsive

--- projet_hafida/view/connexion/login.php
+++ projet_hafida/view/connexion/login.php
@@ -7,11 +7,10 @@
 <!-- Image d'un chat avec un texte alternatif -->
 <img src="public/img/chat.webp" alt="regard d'un chat" class="chat-image">
 
-<div class="login-container">
 <h1>Se connecter</h1> <!-- Titre du formulaire de connexion -->
 
 <!-- Formulaire de connexion -->
-<form id="loginForm" action="index.php?ctrl=security&action=login" method="post" onsubmit="return validateRecaptcha();">
+<form action="index.php?ctrl=security&action=login" method="post" onsubmit="return validateRecaptcha();">
 <!-- Champ pour l'email -->
 <label for="email">Email</label>
 <input type="email" name="email" id="email" value="lea@exemple.com" required><br>
@@ -21,12 +20,13 @@
 <input type="password" name="password" id="password" value="Azerty123456" required><br>
 
 <!-- Widget reCAPTCHA de Google pour la vérification humaine -->
-<div class="login-container">
-<div class="recaptcha" id="recaptcha-container">
+
 <div class="g-recaptcha" data-sitekey="6LeUhkYqAAAAALeAYNXSofNNSW3Fw9vzIRzl4Ngw" data-action="LOGIN"></div>
 <br/>
 <!-- Bouton de soumission du formulaire -->
+<div class="submit-container">
 <input type="submit" name="submitLogin" value="Se connecter">
+</div>
 <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
 
 
