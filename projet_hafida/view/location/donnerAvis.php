5340c0be678debf63d1b512bc8d94fa5fb36356a
Hafida3412 <lounsy81@gmail.com>
2024-10-24 13:57:26 +0200

    correction de la vue avis

--- projet_hafida/view/location/donnerAvis.php
+++ projet_hafida/view/location/donnerAvis.php
@@ -2,7 +2,7 @@
   
 ?>
 
-<h1>Poster un avis pour cette annonce</h1>
+<h1>Poster un avis sur mon expérience</h1>
 
 <form action="index.php?ctrl=location&action=donnerAvis&id=<?= $id?>" method="post">
     <label for="commentaire">Commentaires:</label><br>
